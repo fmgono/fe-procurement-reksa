98b94c9f5fbcd360bfe497b0887c9371a3c9152d
Fathan Margono <fmgonoo@gmail.com>
2020-03-01 08:12:20 +0700

    split router to simple file, and restructure main package at router/index.js to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
-import router from './router'
+import Routes from './routes'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history',
+  base: process.env.BASE_URL
+})
 
 new Vue({
   router,
