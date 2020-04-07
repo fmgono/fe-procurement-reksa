1a6419e78fc19066abe415fece618ad8f8a9f150
Fathan Margono <fmgonoo@gmail.com>
2020-04-07 22:52:48 +0700

    added login page

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,5 +1,39 @@
 <template>
-  <h1>{{ title }}</h1>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Login</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  id="userid"
+                  label="Login"
+                  name="login"
+                  prepend-icon="person"
+                  type="text"
+                ></v-text-field>
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="lock"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
