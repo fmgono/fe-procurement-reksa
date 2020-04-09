36db7d2ebc17e4927d8e379d5861ef46889f622f
Fathan Margono <fmgonoo@gmail.com>
2020-04-09 20:31:34 +0700

    adjust router authentication

--- src/routes.js
+++ src/routes.js
@@ -1,49 +1,32 @@
 import DeliveryOrderForm from '@/pages/DeliveryOrder/DeliveryOrderForm.vue'
 import DeliveryOrderList from '@/pages/DeliveryOrder/DeliveryOrderList.vue'
-// import Dashboard from '@/pages/Dashboard/Dashboard.vue'
+import Dashboard from '@/pages/Dashboard/Dashboard.vue'
 import BaseApp from '@/pages/index.vue'
 import Login from '@/pages/Login/Login.vue'
 
 export default [
   {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
     path: '/',
+    name: 'baseApp',
     component: BaseApp,
     children: [
       {
-        path: '/delivery_order',
+        path: '/',
+        component: Dashboard
+      },
+      {
+        path: 'delivery_order',
         component: DeliveryOrderList
       },
       {
-        path: '/delivery_order/add',
+        path: 'delivery_order/add',
         component: DeliveryOrderForm
       }
     ]
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  // {
-  //   path: '/delivery_order',
-  //   name: 'Delivery Order List',
-  //   component: DeliveryOrderList
-  // },
-  // {
-  //   path: '/delivery_order/add',
-  //   name: 'Delivery Order Entries',
-  //   component: DeliveryOrderForm
-  // },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
-  },
-  {
-    path: '*',
-    redirect: '/login'
   }
 ]
