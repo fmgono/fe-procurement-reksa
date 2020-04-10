e0bb782a8d5b1e5c8d56fe51b874733a1049ff7d
Fathan Margono <fmgonoo@gmail.com>
2020-04-10 15:09:12 +0700

    add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card style="minHeight: '100hv'">
+    <v-container>
+      <div class="display-1 text-center">404 Not Found!</div>
+    </v-container>
+  </v-card>
+</template>
+<script>
+export default {}
+</script>
