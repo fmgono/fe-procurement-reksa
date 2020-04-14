2f675944f30d7ca178f4c75a9454dda5998dca73
Fathan Margono <fmgonoo@gmail.com>
2020-04-14 09:09:35 +0700

    print DO , done

--- src/pages/DeliveryOrder/DeliveryOrderPrint.vue
+++ src/pages/DeliveryOrder/DeliveryOrderPrint.vue
@@ -1,46 +1,71 @@
 <template>
   <div>
     <v-row>
-      <v-col>
-        <v-card outlined tile>
-          <v-card-title>PT BANDAR ELEKTRONIK</v-card-title>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card outlined tile>
-          <v-row>
-            <v-col class="pb-0">
-              <div class="body-1">No PO :</div>
-            </v-col>
-            <v-col class="text-right">
-              <div class="body-1">#1212121</div>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col class="pb-0">
-              <div class="body-1">No DO :</div>
-            </v-col>
-            <v-col class="text-right">
-              <div class="body-1">00007/BE/DO/2020</div>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col class="pb-0">
-              <div class="body-1">Tanggal :</div>
-            </v-col>
-            <v-col class="text-right">
-              <div class="body-1">2020-04-20</div>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card outlined tile>
-          <v-card-title>PT BANDAR ELEKTRONIK</v-card-title>
-        </v-card>
+      <v-col class="pb-0 center-element">
+        <v-card-title>SURAT JALAN / DO</v-card-title>
       </v-col>
     </v-row>
-    <br />
+    <v-row>
+      <v-col class="center-element">
+        <v-card outlined tile>
+          <v-card-title>PT BANDAR ELEKTRONIK</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col>
+        <!-- <v-card outlined tile> -->
+        <v-row>
+          <v-col class="pb-0">
+            <div class="body-1">No PO :</div>
+          </v-col>
+          <v-col class="text-right">
+            <div class="body-1">{{data.po_seq}}</div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="pb-0">
+            <div class="body-1">No DO :</div>
+          </v-col>
+          <v-col class="text-right">
+            <div class="body-1">{{data.do_seq}}</div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="pb-0">
+            <div class="body-1">Tanggal :</div>
+          </v-col>
+          <v-col class="text-right">
+            <div class="body-1">{{data.do_date}}</div>
+          </v-col>
+        </v-row>
+        <!-- </v-card> -->
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-col class="pb-0">
+            <div class="body-1">Kepada YTH:</div>
+          </v-col>
+          <v-col class="text-right">
+            <div class="body-1">PT Reksa Nasional</div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="pb-0" cols="2">
+            <div class="body-1">Alamat:</div>
+          </v-col>
+          <v-col class="text-right" cols="10">
+            <div class="body-1">Graha Rekso Building 5th floor</div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="pb-0" cols="2">
+            <div class="body-1"></div>
+          </v-col>
+          <v-col class="text-right mt-5" cols="10">
+            <hr />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
     <v-simple-table light class="table-print">
       <template v-slot:default>
         <thead>
@@ -53,51 +78,84 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>RUCIKA PIPA - 123123131</td>
-            <td>2</td>
-            <td>UNIT</td>
-            <td>PIPA RUCIKA</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>RUCIKA PIPA - 123123131</td>
-            <td>2</td>
-            <td>UNIT</td>
-            <td>PIPA RUCIKA</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>RUCIKA PIPA - 123123131</td>
-            <td>2</td>
-            <td>UNIT</td>
-            <td>PIPA RUCIKA</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>RUCIKA PIPA - 123123131</td>
-            <td>2</td>
-            <td>UNIT</td>
-            <td>PIPA RUCIKA</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>RUCIKA PIPA - 123123131</td>
-            <td>2</td>
-            <td>UNIT</td>
-            <td>PIPA RUCIKA</td>
+          <tr v-for="item in data.do_detail" :key="item.do_rownum">
+            <td>{{ item.do_rownum }}</td>
+            <td>{{ item.do_itemid }}</td>
+            <td>{{ item.do_qty }}</td>
+            <td>{{ item.do_satuan }}</td>
+            <td>{{ item.do_deskripsi }}</td>
           </tr>
         </tbody>
       </template>
     </v-simple-table>
+    <br />
+    <v-row no-gutters>
+      <v-col>
+        <v-card outlined tile>
+          <v-card-text class="text-center">Dikirim Oleh</v-card-text>
+          <!-- <v-card-text>&nbsp;</v-card-text> -->
+          <v-card-text>&nbsp;</v-card-text>
+          <v-card-text class="pb-0">Nama &nbsp; :</v-card-text>
+          <v-card-text class="pt-2">Tanggal :</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card outlined tile>
+          <v-card-text class="text-center">Diketahui Oleh</v-card-text>
+          <!-- <v-card-text>&nbsp;</v-card-text> -->
+          <v-card-text>&nbsp;</v-card-text>
+          <v-card-text class="pb-0">Nama &nbsp; :</v-card-text>
+          <v-card-text class="pt-2">Tanggal :</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card outlined tile>
+          <v-card-text class="text-center">Diterima Oleh</v-card-text>
+          <!-- <v-card-text>&nbsp;</v-card-text> -->
+          <v-card-text>&nbsp;</v-card-text>
+          <v-card-text class="pb-0">Nama &nbsp; :</v-card-text>
+          <v-card-text class="pt-2">Tanggal :</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
-export default {}
+import axios from 'axios'
+
+const { token } = JSON.parse(localStorage.getItem('userAuth'))
+
+export default {
+  data: () => ({
+    data: {}
+  }),
+  created() {
+    axios
+      .post(`${process.env.VUE_APP_BASE_API_URL}api/do/prints`, {
+        do_seq: this.$route.params.doNo,
+        token
+      })
+      .then(resp => {
+        this.data = resp.data.data
+      })
+      .catch(e => console.error(e))
+  }
+}
 </script>
 <style scoped>
 .table-print {
   border: thin solid rgba(0, 0, 0, 0.12);
+}
+
+.center-element {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media print {
+  .margin-wrapper {
+    margin-top: -110px;
+  }
 }
 </style>
