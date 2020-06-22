0a1e2280fbcb47fe91372209c2432d425355d38f
Fathan Margono <fmgonoo@gmail.com>
2020-06-22 22:35:23 +0700

    penyesuaian cetakan invoice

--- src/pages/Invoice/Print.vue
+++ src/pages/Invoice/Print.vue
@@ -1,69 +1,82 @@
 <template>
   <div class="print-invoice">
     <v-row>
-      <v-col class="pb-0 center-element">
-        <v-card-title>INVOICE</v-card-title>
+      <v-col class="pb-0 mt-2 center-element">
+        <v-card-title class="invoice-text">INVOICE</v-card-title>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
         <!-- <v-card outlined tile> -->
         <v-row>
-          <v-col class="pb-0" cols="3">
+          <v-col class="margin-right pb-0" cols="2">
             <div class="text-caption">Date</div>
           </v-col>
-          <v-col class="pb-0 text-right">
-            <div class="text-caption">{{ data.inv_date }} 121</div>
+          <!-- <v-col cols="1" class="pb-0 text-right">:</v-col> -->
+          <v-col class="text-left pb-0">
+            <div class="text-caption">: {{ data.inv_date_fmt }}</div>
           </v-col>
-          <!-- <v-spacer class="d-print-none"></v-spacer> -->
+          <v-spacer class="d-print-none"></v-spacer>
           <!-- <v-spacer></v-spacer> -->
+          <v-spacer></v-spacer>
         </v-row>
         <v-row>
-          <v-col class="pb-0 pt-0" cols="3">
+          <v-col class="margin-right pb-0 pt-0" cols="2">
             <div class="text-caption">Company ID</div>
           </v-col>
-          <v-col class="text-right pt-0 pb-0">
-            <div class="text-caption font-weight-black">PT Rekso Nasional Food</div>
+          <!-- <v-col cols="1" class="text-right pt-0 pb-0">:</v-col> -->
+          <v-col class="text-left pt-0 pb-0">
+            <div class="text-caption">
+              :
+              <span class="font-weight-black">PT Rekso Nasional Food</span>
+            </div>
           </v-col>
-          <!-- <v-spacer class="d-print-none"></v-spacer> -->
+          <v-spacer class="d-print-none"></v-spacer>
           <!-- <v-spacer></v-spacer> -->
+          <v-spacer></v-spacer>
         </v-row>
         <v-row>
-          <v-col class="pb-0 pt-0" cols="3">
+          <v-col class="margin-right pb-0 pt-0" cols="2">
             <div class="text-caption">Invoice</div>
           </v-col>
-          <v-col class="text-right pt-0 pb-0">
-            <div class="text-caption">{{ $route.params.invNo }}</div>
+          <!-- <v-col cols="1" class="text-right pt-0 pb-0">:</v-col> -->
+          <v-col class="text-left pt-0 pb-0">
+            <div class="text-caption">: {{ $route.params.invNo }}</div>
           </v-col>
-          <!-- <v-spacer class="d-print-none"></v-spacer> -->
+          <v-spacer class="d-print-none"></v-spacer>
           <!-- <v-spacer></v-spacer> -->
+          <v-spacer></v-spacer>
         </v-row>
         <v-row>
-          <v-col class="pb-0 pt-0" cols="3">
+          <v-col class="margin-right pb-0 pt-0" cols="2">
             <div class="text-caption">PO No</div>
           </v-col>
-          <v-col class="text-right pt-0 pb-0">
-            <div class="text-caption">{{ data.po_seq }}</div>
+          <!-- <v-col cols="1" class="text-right pt-0 pb-0">:</v-col> -->
+          <v-col class="text-left pt-0 pb-0">
+            <div class="text-caption">: {{ data.po_seq }}</div>
           </v-col>
-          <!-- <v-spacer class="d-print-none"></v-spacer> -->
+          <v-spacer class="d-print-none"></v-spacer>
           <!-- <v-spacer></v-spacer> -->
+          <v-spacer></v-spacer>
         </v-row>
         <v-row>
-          <v-col class="pb-0 pt-0" cols="3">
+          <v-col class="margin-right pb-0 pt-0" cols="2">
             <div class="text-caption">Kwitansi No</div>
           </v-col>
-          <v-col class="text-right pt-0">
-            <div class="text-caption">{{ data.kwitansi_seq }}</div>
+          <!-- <v-col cols="1" class="text-right pt-0">:</v-col> -->
+          <v-col class="text-left pt-0">
+            <div class="text-caption">: {{ data.kwitansi_seq }}</div>
           </v-col>
-          <!-- <v-spacer class="d-print-none"></v-spacer> -->
+          <v-spacer class="d-print-none"></v-spacer>
           <!-- <v-spacer></v-spacer> -->
+          <v-spacer></v-spacer>
         </v-row>
         <!-- </v-card> -->
       </v-col>
-      <v-col class="center-element column-direction" cols="5">
+      <!-- <v-col class="center-element column-direction" cols="5">
         <img :src="logo" alt="logo" class="center" />
         <div class="subtitle-1">CV BANDAR ELEKTRONIK</div>
-      </v-col>
+      </v-col>-->
     </v-row>
     <v-row>
       <v-col class="pt-0">
@@ -106,9 +119,9 @@
         <thead>
           <tr>
             <th>No</th>
-            <th>Deskripsi</th>
-            <th>Kode Item</th>
-            <th>Qty</th>
+            <th>QTY</th>
+            <th>Type / Item</th>
+            <th>Description</th>
             <th class="text-right">Unit Price</th>
             <th class="text-right">Total</th>
           </tr>
@@ -116,23 +129,26 @@
         <tbody>
           <tr v-for="item in data.inv_detail" :key="item.inv_rownum">
             <td>{{ item.inv_rownum }}</td>
-            <td>{{ item.deskripsi_barang }}</td>
-            <td>{{ item.inv_itemid }}</td>
             <td>{{ item.inv_qty }}</td>
+            <td>{{ item.inv_itemid }}</td>
+            <td>{{ item.deskripsi_barang }}</td>
             <td class="text-right">{{ item.inv_cost }}</td>
             <td class="text-right">{{ item.total_cost }}</td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
-            <td colspan="4" class="subtitle-2">Grandtotal</td>
-            <td class="subtitle-2 text-right">{{data.total_cost}}</td>
+            <td colspan="2" class="subtitle-2">Grandtotal</td>
+            <td colspan="4" class="subtitle-2 text-right">{{data.total_cost}}</td>
           </tr>
           <tr>
-            <td colspan="2" class="text-center text-caption font-weight-black">Terbilang</td>
             <td
               colspan="3"
-              class="capitalize text-right text-caption font-weight-black font-italic"
+              class="terbilang-left text-left text-caption font-weight-black"
+            >Terbilang</td>
+            <td
+              class="terbilang-right text-right capitalize text-caption font-weight-black font-italic"
+              colspan="3"
             >{{data.terbilang}}</td>
           </tr>
         </tfoot>
@@ -149,7 +165,7 @@
         </v-row>
         <v-row>
           <v-col class="pb-0 pt-1">
-            <div class="text-caption">Jatuh Tempo Pembayaran</div>
+            <div class="text-caption">Jatuh Tempo Pembayaran 30(tiga puluh) hari</div>
           </v-col>
         </v-row>
         <v-row>
@@ -227,12 +243,10 @@ export default {
       })
       .then(resp => {
         this.data = resp.data.data
-        this.data.total_cost = `Rp. ${this.data.total_cost.toLocaleString(
-          'id'
-        )}`
+        this.data.total_cost = `${this.data.total_cost.toLocaleString('id')}`
         this.data.inv_detail = this.data.inv_detail.map(item => {
-          item.inv_cost = `Rp. ${item.inv_cost.toLocaleString('id')}`
-          item.total_cost = `Rp. ${item.total_cost.toLocaleString('id')}`
+          item.inv_cost = `${item.inv_cost.toLocaleString('id')}`
+          item.total_cost = `${item.total_cost.toLocaleString('id')}`
           return item
         })
       })
@@ -240,12 +254,36 @@ export default {
 }
 </script>
 <style scoped>
+.invoice-text {
+  text-decoration: underline;
+}
+
 .print-invoice {
   font-size: 12px !important;
 }
 
-.table-print {
-  border: thin solid rgba(0, 0, 0, 0.63);
+.margin-right {
+  margin-right: -54px;
+}
+
+.table-print,
+.table-print table,
+.table-print th,
+.table-print td {
+  border: 1px solid rgba(12, 12, 12, 0.932);
+}
+
+.table-print th {
+  color: rgba(12, 12, 12, 0.932) !important;
+}
+
+/* .table-print tfoot, */
+.terbilang-left {
+  border-right: none !important;
+}
+
+.terbilang-right {
+  border-left: none !important;
 }
 
 .center-element {
